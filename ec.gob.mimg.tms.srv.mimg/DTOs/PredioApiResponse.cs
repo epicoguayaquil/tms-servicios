0ce181b3566b66d29a9fbd1c872e6644f6f633c5
Juan Lafuente <jlafuente@datafast.com.ec>
2023-01-19 00:47:07 -0500

    Api Integracion Predios

---
+++ ec.gob.mimg.tms.srv.mimg/DTOs/PredioApiResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using ec.gob.mimg.tms.srv.mimg.Models;
+
+namespace ec.gob.mimg.tms.srv.mimg.DTOs
+{
+	public class PredioApiResponse
+    {
+        public PredioModel DataResult { get; set; }
+
+    }
+}
+
