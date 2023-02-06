572e21e8af2945d4b74024e63ab8ad20a20dfcf1
Juan Lafuente <jlafuente@telconet.ec>
2023-02-06 14:15:54 -0500

    Load Files Webservice

---
+++ ec.gob.mimg.tms.api/DTOs/Response/FileResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace ec.gob.mimg.tms.api.DTOs.Response
+{
+    public class FileResponse
+    {
+        public FileModel Post { get; set; }
+    }
+
+    public class FileModel
+    {
+        public string RUC { get; set; }
+        public string Description { get; set; }
+        public string Imagepath { get; set; }
+    }
+}
